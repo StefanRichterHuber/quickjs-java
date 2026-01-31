3ff908269c262367367a4c95350e0dabd273b296
Stefan Richter-Huber <stefan@richter-huber.de>
2026-01-31 10:56:14 +0100

    Added some java doc and equals for QuickJSObjectEnry

--- src/main/java/io/github/stefanrichterhuber/quickjs/internal/QuickJSObjectEntry.java
+++ src/main/java/io/github/stefanrichterhuber/quickjs/internal/QuickJSObjectEntry.java
@@ -1,6 +1,7 @@
 package io.github.stefanrichterhuber.quickjs.internal;
 
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import io.github.stefanrichterhuber.quickjs.QuickJSObject;
 
@@ -8,11 +9,22 @@ import io.github.stefanrichterhuber.quickjs.QuickJSObject;
  * Entry implementation for QuickJSObject. Keys are copied from the JS object
  * when the entry is created. Values are copied from the JS object when the
  * entry is accessed.
+ * 
+ * @param <K> the type of keys maintained by this map. Must be a String, Number
+ *            or Boolean.
+ * @param <V> the type of mapped values. Any type that can be converted to a
+ *            QuickJS value (including other maps)
  */
 public class QuickJSObjectEntry<K, V> implements Entry<K, V> {
     private final QuickJSObject<K, V> parent;
     private final K key;
 
+    /**
+     * Creates a QuickJSObjectEntry for the given QuickJSObject and key.
+     * 
+     * @param parent parent QuickJSObject
+     * @param key    key of the entry
+     */
     public QuickJSObjectEntry(QuickJSObject<K, V> parent, K key) {
         this.parent = parent;
         this.key = key;
@@ -35,11 +47,25 @@ public class QuickJSObjectEntry<K, V> implements Entry<K, V> {
         return oldValue;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !(o instanceof Entry)) {
+            return false;
+        }
+        final Entry<?, ?> that = (Entry<?, ?>) o;
+        return Objects.equals(getKey(), that.getKey()) && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
     public int hashCode() {
         final V value = this.getValue();
         return (this.key == null ? 0 : this.key.hashCode()) ^ (value == null ? 0 : value.hashCode());
     }
 
+    @Override
     public String toString() {
         return this.key + "=" + this.getValue();
     }
